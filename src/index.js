5d1b422c680fcc148757b0718da0a07c42758397
Ben Kiggen and Talia Haller <ben@>
2018-09-20 14:42:32 -0700

    add css animations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import './sass/styles.scss';
+import $ from 'jquery';
 
-console.log("hey!");
+$(document).ready(function(){
+  $( ".slogan" ).animate({
+    opacity: 1,
+    left: "-=50",
+    height: "show"
+  }, 2000, function() {
+  });
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 550) {
+      $( ".right-bar-1-items" ).animate({
+        opacity: 1,
+        // left: "-=50",
+        height: "show"
+      }, 2000, function() {
+      });
+    }
+  });
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 850) {
+      $(".aboutPicture").show();
+      $(".right-bar-about").show();
+      $( ".aboutPicture" ).addClass("animated slideInLeft");
+      $( ".right-bar-about").addClass("animated slideInRight delay-3s");
+    }
+  });
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 850) {
+      $(".aboutPicture").show();
+      $(".right-bar-about").show();
+      $( ".aboutPicture" ).addClass("animated slideInLeft");
+      $( ".right-bar-about").addClass("animated slideInRight delay-3s");
+    }
+  });
+
+
+
+
+
+  });
